578730b62bbc27815d35cd49dcae6f7d7d9a9888
Mariam Jananashvili <mariam.jananashvili@gau.edu.ge>
2026-02-25 13:13:00 +0400

    Add files via upload

---
+++ mariami/lecture2/mari.php
@@ -0,0 +1,12 @@
+ <?php
+ echo "<h1> Lecture 2</h2>";
+ $arr_1=[34,5.8,"Gau",False,"i am student"];
+ echo"<pre>";
+ print_r($arr_1);
+ echo "</pre>";
+ $arr_asoc=['name'=>"mariami",'age'=>21,"subject"=>"PHP web Technologies"];
+  echo"<pre>";
+ print_r($arr_asoc);
+ echo "</pre>";
+ 
+ ?>
